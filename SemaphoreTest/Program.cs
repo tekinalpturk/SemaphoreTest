118adbc6ab4f361628e90522e1e1effcc82be2b5
Tekin Alptürk <tekin.alpturk@setur.com.tr>
2022-11-14 23:26:10 +0300

    Console app and API is created with parallel call that causes error

---
+++ SemaphoreTest/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Net.Http.Headers;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.Json;
+using System.Text;
+using System.Diagnostics;
+
+Console.WriteLine("Program Started");
+
+using HttpClient httpClient = new();
+
+int[] arrayToCheck = Enumerable.Range(1, 1000).ToArray();
+
+
+
+Console.WriteLine($"Total number of numbers to check = {arrayToCheck.Count()}");
+Stopwatch sw = new Stopwatch();
+sw.Start();
+
+var semaphore = new SemaphoreSlim(initialCount: 1, maxCount: 1);
+var tasks = arrayToCheck.Select(async item =>
+{
+    await SendApiRequest(item);
+});
+await Task.WhenAll(tasks);
+
+sw.Stop();
+Console.WriteLine($"Time elapsed = {sw.Elapsed}");
+Console.WriteLine("Program Finished");
+Console.ReadLine();
+
+
+async Task SendApiRequest(int number)
+{
+    try
+    {
+        //Send HTTP request and get the response
+        var response = await httpClient.GetAsync($"https://localhost:7009/api/Math/IsPrime?number={number}");
+        var responseAsString = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"{number} {(bool.Parse(responseAsString) ? "is Prime" : "is not Prime")}"); 
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{number} - {ex.Message}");
+    }
+}
---
+++ SimpleAPI/Controllers/MathController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> IsPrime(int number)
+        {
+            Thread.Sleep(500);
+            if (number <= 1) return Ok(false);
+            if (number == 2) return Ok(true);
+            if (number % 2 == 0) return Ok(false);
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return Ok(false);
+
+            return Ok(true);
+        }
+    }
+}
